6c14918ad977553faab2083b481895c2b1104b67
Eslam Magdy <eslam3400@gmail.com>
2021-07-25 15:31:12 +0200

    finished with the basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Square = (props) => (
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+  </button>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: Array(9).fill(null),
+      xTurn: true
+    };
+  }
+
+  winner(fields) {
+    const condetions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 8],
+    ];
+    const limit = condetions.length
+    for (let index = 0; index < limit; index++) {
+      const [a, b, c] = condetions[index];
+      if (fields[a] && fields[a] === fields[b] && fields[a] === fields[c]) return fields[a];
+    }
+    return null;
+  }
+
+  handleClick(i) {
+    const fields = [...this.state.fields];
+    if (this.winner(fields) || fields[i]) return;
+    fields[i] = this.state.xTurn ? 'X' : 'O';
+    this.setState({ fields, xTurn: !this.state.xTurn });
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.fields[i]} onClick={() => this.handleClick(i)} />;
+  }
+
+  render() {
+    const winner = this.winner(this.state.fields)
+    let status = winner ? `Winner: ${winner}` : 'Next player: ' + (this.state.xTurn ? 'X' : 'O')
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
